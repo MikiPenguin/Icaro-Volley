6c768c5e4f6c9b9996aa50d39c2f20cadada57dc
miki@penguin <96893972+MikiPenguin@users.noreply.github.com>
2023-05-09 16:24:25 +0200

    Update menu.js

--- main/menu.js
+++ main/menu.js
@@ -1,6 +1,8 @@
-document.addEventListener("click", function(e) {
+document.addEventListener("mouseover", function(e) {
   var submenu = document.querySelector(".submenu");
-  if (submenu.style.display === "block" && !submenu.contains(e.target)) {
+  if (e.target.matches(".button-squ")) {
+    submenu.style.display = "block";
+  } else if (submenu.style.display === "block" && !submenu.contains(e.target)) {
     submenu.style.display = "none";
   }
 });
